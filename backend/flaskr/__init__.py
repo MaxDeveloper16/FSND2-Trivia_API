f9796c61ad51ab68eed30074414d64a3257178ae
Max <maxdeveloper16@gmail.com>
2020-06-17 19:22:55 -0400

    Implement question add and delete functions

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -9,64 +9,69 @@ from models import setup_db, Question, Category
 
 QUESTIONS_PER_PAGE = 10
 
+
 def create_app(test_config=None):
-    app = Flask(__name__)
-    setup_db(app)
-     #Set up CORS. Allow '*' for origins
-    cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+  app = Flask(__name__)
+  setup_db(app)
+  # Set up CORS. Allow '*' for origins
+  cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    # CORS Headers 
-    @app.after_request
-    def after_request(response):
-        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS')
-        return response
-    
-    @app.route('/')
-    def hello():
-        return jsonify({'message':'Hello, World!'})
+  # CORS Headers
+  @app.after_request
+  def after_request(response):
+    response.headers.add('Access-Control-Allow-Headers',
+                          'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods',
+                          'GET,POST,DELETE,OPTIONS')
+    return response
 
-    #Get all categories to Json
-    @app.route('/api/categories')
-    def get_categories():
-      try:         
+  # Get all categories to Json
+  @app.route('/api/categories')
+  def get_categories():
+    try:
         return jsonify(get_all_categories())
-      except Exception as e:
+    except Exception as e:
         logging.exception(e)
 
-    #Get all categories
-    def get_all_categories():
-      try: 
+  # Get all categories
+
+  def get_all_categories():
+    try:
         categories = Category.query.all()
-        format_categories = [categorie.format() for categorie in categories]
+        format_categories = [categorie.format()
+                              for categorie in categories]
         return format_categories
-      except Exception as e:
+    except Exception as e:
         logging.exception(e)
 
-    def filter_questions(category_id=None):
-      try:
+  # Get questions by searchTerm or category_id  including pagination
+
+  def filter_questions(category_id=None):
+    try:
         search_term = request.args.get('searchTerm')
         page = request.args.get("page", 1, type=int)
         start = (page - 1) * QUESTIONS_PER_PAGE
         end = start + QUESTIONS_PER_PAGE
 
         if search_term:
-          questions = Question.query.filter(
-            Question.question.ilike(f"%{search_term}%")
+            questions = Question.query.filter(
+                Question.question.ilike(f"%{search_term}%")
             ).all()
 
         elif category_id:
-          questions = Question.query.filter_by(category=str(category_id)).all()
+            questions = Question.query.filter_by(
+                category=str(category_id)).all()
         else:
-          questions = Question.query.all()
+            questions = Question.query.all()
 
         total_questions = len(questions)
-        format_quesions = [question.format() for question in questions][start:end]
+        format_quesions = [question.format()
+                            for question in questions][start:end]
 
         if(total_questions == 0):
             abort(404)
 
-        #Return a list of questions
+        # Return a list of questions
         return jsonify({
             'success': True,
             'questions': format_quesions,
@@ -74,85 +79,69 @@ def create_app(test_config=None):
             'current_category': category_id if category_id else 0,
             'categories': get_all_categories()
         })
-      except Exception as e:
+    except Exception as e:
         logging.exception(e)
 
-    #Get all questions
-    @app.route('/api/questions')
-    def get_questions():
-      return filter_questions()
+  # Get all questions
 
-    @app.route('/api/categories/<int:category_id>/questions')
-    def get_questions_by_category(category_id):
-      return filter_questions(category_id)
-    
-    return app
+  @app.route('/api/questions')
+  def get_questions():
+    return filter_questions()
 
-#   '''
-#   TEST: At this point, when you start the application
-#   you should see questions and categories generated,
-#   ten questions per page and pagination at the bottom of the screen for three pages.
-#   Clicking on the page numbers should update the questions. 
-#   '''
+  # Get questions by category_id
+  @app.route('/api/categories/<int:category_id>/questions')
+  def get_questions_by_category(category_id):
+    return filter_questions(category_id)
 
-#   '''
-#   @TODO: 
-#   Create an endpoint to DELETE question using a question ID. 
+  # POST a new question
+  @app.route('/api/questions', methods=['POST'])
+  def add_question():
+    data = request.get_json()
 
-#   TEST: When you click the trash icon next to a question, the question will be removed.
-#   This removal will persist in the database and when you refresh the page. 
-#   '''
+    question = data.get("question", None)
+    answer = data.get("answer", None)
+    difficulty = data.get("difficulty", None)
+    category = data.get("category", None)
 
-#   '''
-#   @TODO: 
-#   Create an endpoint to POST a new question, 
-#   which will require the question and answer text, 
-#   category, and difficulty score.
+    if not all([question, answer, category, difficulty]):
+        return abort(400)
 
-#   TEST: When you submit a question on the "Add" tab, 
-#   the form will clear and the question will appear at the end of the last page
-#   of the questions list in the "List" tab.  
-#   '''
+    try:
+        question = Question(
+          question=question,
+          answer=answer,
+          category=category,
+          difficulty=difficulty
+        )
+        question.insert()
 
-#   '''
-#   @TODO: 
-#   Create a POST endpoint to get questions based on a search term. 
-#   It should return any questions for whom the search term 
-#   is a substring of the question. 
+        return filter_questions(category)
+    except Exception as e:
+        logging.exception(e)
+        return abort(500)
 
-#   TEST: Search by any phrase. The questions list will update to include 
-#   only question that include that string within their question. 
-#   Try using the word "title" to start. 
-#   '''
+  #Delete a question
+  @app.route('/api/questions/<int:question_id>',methods=['DELETE'])
+  def delete_question(question_id):
+    try:
+        # delete question
+        question = Question.query.filter(
+            Question.id == question_id
+        ).one_or_none()
 
-#   '''
-#   @TODO: 
-#   Create a GET endpoint to get questions based on category. 
+        if question is None:
+            abort(404)
 
-#   TEST: In the "List" tab / main screen, clicking on one of the 
-#   categories in the left column will cause only questions of that 
-#   category to be shown. 
-#   '''
+        question.delete()
+
+        # return response
+        return jsonify({
+            'success': True,
+            'deleted_id': question.id
+        })
+    except Exception as e:
+        logging.exception(e)
+        return abort(500)
 
 
-#   '''
-#   @TODO: 
-#   Create a POST endpoint to get questions to play the quiz. 
-#   This endpoint should take category and previous question parameters 
-#   and return a random questions within the given category, 
-#   if provided, and that is not one of the previous questions. 
-
-#   TEST: In the "Play" tab, after a user selects "All" or a category,
-#   one question at a time is displayed, the user is allowed to answer
-#   and shown whether they were correct or not. 
-#   '''
-
-#   '''
-#   @TODO: 
-#   Create error handlers for all expected errors 
-#   including 404 and 422. 
-#   '''
-
-    return app
-
-    
+  return app
